dbea3466fd22939ab9aa00ac35984bf39957b0a4
edwinjordan <edwin.sekawanmedia@gmail.com>
2022-08-15 23:17:19 +0700

    Update schema.role.go

--- schemas/schema.role.go
+++ schemas/schema.role.go
@@ -1,6 +1,6 @@
 package schemas
 
 type SchemaRole struct {
-	Role_id   string `json:"user_id" validated:"uuid"`
+	Role_id   string `json:"Role_id" validated:"uuid"`
 	Role_name string `json:"Role_name" validate:"required,lowercase"`
 }
